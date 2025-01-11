640e506745a398b89bf290dbb6f53c51448cacd0
PreSuffix <contato@presuffix.net>
2025-01-11 20:41:20 -0300

    fixes cookie bug

--- src/handlers/apiHandler.js
+++ src/handlers/apiHandler.js
@@ -14,13 +14,18 @@ module.exports = (mongoUrl) => {
 
     app.use(cors({ origin: ['http://localhost:8088'], credentials: true }));
 
+    let domain = (new URL(process.env.DASHBOARD_URL)).host.split(".");
+
+    if(domain.length > 2) domain = `.${domain[domain.length - 2]}.${domain[domain.length - 1]}`;
+    else domain = "." + domain.join(".");
+
     app.use(session({
         secret: process.env.API_SECRET, 
         resave: false, 
         saveUninitialized: false,
         cookie: {
             maxAge: 60000 * 60 * 24 * 30,
-            domain: (new URL(process.env.DASHBOARD_URL)).host
+            domain
         },
         store: store.create({
             mongoUrl
